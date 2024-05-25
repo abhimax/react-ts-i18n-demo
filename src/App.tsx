c355b66adaeae4dc65bd8072cb4d755599936826
Abhiman Ranaweera <abhiman.max@gmail.com>
2024-05-26 00:04:08 +0530

    feat:add form submit  functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
+import { useState } from 'react';
 import './App.css'
 import ContactForm from './components/ContactForm';
 import LanguageSwitcher from './components/LanguageSwitcher'
 import { useTranslation } from 'react-i18next'
 
 function App() {
+  interface useType {
+    name: string,
+    message: string
+  }
   const { t } = useTranslation();
-
+  const [user, setUser] = useState<useType>();
+  const handleOnSubmit = (name: string, message:string) => {
+    setUser({"name":name,"message":message});
+  }
   return (
     <>
-      <LanguageSwitcher/>
+<LanguageSwitcher />
       <main>
-        <h1>{t('welcome')}</h1>
-        <p>{t('description')}</p>
-        <ContactForm />
+        {user && <h2 data-test-id="greeting">{`Hi, ${user.name}`}</h2>}
+        <h1 data-test-id="welcome-message">{t('welcome')}</h1>
+        <p data-test-id="description">{t('description')}</p>
+        <ContactForm onSubmit={handleOnSubmit} />
+        {user && (
+          <div className="contact-details" data-test-id="contact-details">
+            <h2>{t('Submitted Information')}</h2>
+            <p><strong>{t('name')}:</strong> {user.name}</p>
+            <p><strong>{t('message')}:</strong> {user.message}</p>
+          </div>
+        )}
       </main>
     </>
   )
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import './ContactForm.css'
 
-const ContactForm: React.FC = () => {
+interface contactFormProps {
+  onSubmit: (name: string, message: string) => void
+}
+const ContactForm: React.FC <contactFormProps> = ({onSubmit}) => {
   const { t } = useTranslation()
+  const [name, setName] = useState("")
+  const [message, setMessage] = useState("")
 
+  const onHandleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    onSubmit(name, message);
+
+  }
   return (
-    <form className="contact-form">
+    <form className="contact-form" onSubmit={onHandleSubmit}>
       <label className="contact-form__label">
         {t('name')}
-        <input type="text" name="name" className="contact-form__input" required />
+        <input type="text" name="name" className="contact-form__input" required onChange={(e) => {setName(e.target.value)}}/>
       </label>
       <label className="contact-form__label">
         {t('message')}
-        <textarea name="message" className="contact-form__textarea" required></textarea>
+        <textarea name="message" className="contact-form__textarea" required onChange={(e) => { setMessage(e.target.value)}}></textarea>
       </label>
       <button type="submit" className="contact-form__button">{t('submit')}</button>
     </form>
